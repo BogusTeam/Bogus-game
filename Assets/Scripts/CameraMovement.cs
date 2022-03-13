c22222f7b26c23f48d6aeccc8b29ed58ba48cd11
Druzai <g9code@yandex.ru>
2022-03-13 20:17:32 +0300

    Added changing camera position and angle via mouse scroll wheel

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,16 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
+    private readonly List<int> _localCameraPositions = new() { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+    private int _currentCameraPos = 1;
+
+    private Camera _cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.mouseScrollDelta.y != 0 &&
+            (Input.mouseScrollDelta.y < 0 && _currentCameraPos < _localCameraPositions.Last() ||
+             Input.mouseScrollDelta.y > 0 && _currentCameraPos > _localCameraPositions.First()))
+        {
+            var pos = _cam.transform.localPosition;
+            pos.y -= 6 * Input.mouseScrollDelta.y;
+            pos.z += 2 * Input.mouseScrollDelta.y;
+            _cam.transform.localPosition = pos;
+            _cam.transform.Rotate(Input.mouseScrollDelta.y * -3, 0, 0);
+            _currentCameraPos -= (int)Input.mouseScrollDelta.y;
+            Debug.Log(_currentCameraPos);
+        }
         
+        // if (Input.mouseScrollDelta.y != 0)
+        // {
+        //     var rot = _cam.transform.localRotation;
+        //     Debug.Log($"{rot.w}, {rot.x}, {rot.y}, {rot.z}");
+        //
+        //     var pos2 = _cam.transform.position;
+        //     Debug.Log($"{pos2.x}, {pos2.y}, {pos2.z}");
+        //
+        //     var pos3 = _cam.transform.localPosition;
+        //     Debug.Log($"{pos3.x}, {pos3.y}, {pos3.z}");
+        // }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,7 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    private UnityEngine.Camera _cam;
-    private GameObject _obj;
+    private Camera _cam;
     private NavMeshAgent _agent;
     private CharacterController _controller;
     private Animator _animator;
@@ -16,33 +15,30 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _cam = UnityEngine.Camera.main;
-        _obj = GameObject.FindWithTag("Player");
+        _cam = Camera.main;
         _agent = GetComponent<NavMeshAgent>();
-        // _controller = GetComponent<CharacterController>();
         _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // clicking on the nav mesh, sets the destination of the agent and off he goes
+        // Clicking on the nav mesh, sets the destination of the agent and off he goes
         if (Input.GetMouseButtonDown(0))
         {
             if (Physics.Raycast(_cam.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100))
             {
                 _agent.SetDestination(hit.point);
                 _animator.Play(_walkingAnimation);
-                // _controller.Move(hit.point);
-                Debug.Log(_walkingAnimation);
+                // Debug.Log(_walkingAnimation);
             }
         }
 
-        var delta = _agent.destination - _obj.transform.position;
-        if (Math.Abs(delta.x) < 0.2 || Math.Abs(delta.z) < 0.2)
+        var delta = _agent.destination - gameObject.transform.position;
+        if (Math.Abs(delta.x) < 0.08 || Math.Abs(delta.z) < 0.08)
         {
             _animator.Play(_idleAnimation);
-            Debug.Log(_idleAnimation);
+            // Debug.Log(_idleAnimation);
         }
     }
 }
