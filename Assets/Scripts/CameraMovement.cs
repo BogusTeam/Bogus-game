2db258f20eec9ade4a5eb19dca43bc5bf07c10b7
Druzai <g9code@yandex.ru>
2022-03-15 23:43:22 +0300

    Added moving camera around player

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    public float cameraYAxisSpeed = 6.0f;
+    public float cameraZAxisSpeed = 2.0f;
+    public float cameraXAngleBevel = 3.0f;
+    public float cameraLookSpeed = 10.0f;
+    public GameObject player;
+
     private readonly List<int> _localCameraPositions = new() { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
     private int _currentCameraPos = 1;
-
     private Camera _cam;
 
     // Start is called before the first frame update
@@ -22,25 +27,25 @@ public class CameraMovement : MonoBehaviour
             (Input.mouseScrollDelta.y < 0 && _currentCameraPos < _localCameraPositions.Last() ||
              Input.mouseScrollDelta.y > 0 && _currentCameraPos > _localCameraPositions.First()))
         {
-            var pos = _cam.transform.localPosition;
-            pos.y -= 6 * Input.mouseScrollDelta.y;
-            pos.z += 2 * Input.mouseScrollDelta.y;
-            _cam.transform.localPosition = pos;
-            _cam.transform.Rotate(Input.mouseScrollDelta.y * -3, 0, 0);
+            // var pos = _cam.transform.localPosition;
+            // pos.y -= 6 * Input.mouseScrollDelta.y;
+            // pos.z += 2 * Input.mouseScrollDelta.y;
+            // _cam.transform.localPosition = pos;
+            // TODO: Replace Translate with func without side effects
+            _cam.transform.Translate(new Vector3(0, 10 * cameraYAxisSpeed * -Input.mouseScrollDelta.y,
+                10 * cameraZAxisSpeed * Input.mouseScrollDelta.y) * Time.deltaTime);
+            _cam.transform.Rotate(Input.mouseScrollDelta.y * -cameraXAngleBevel, 0, 0);
             _currentCameraPos -= (int)Input.mouseScrollDelta.y;
-            Debug.Log(_currentCameraPos);
+            Debug.Log($"Current Camera Position: {_currentCameraPos}");
         }
-        
-        // if (Input.mouseScrollDelta.y != 0)
-        // {
-        //     var rot = _cam.transform.localRotation;
-        //     Debug.Log($"{rot.w}, {rot.x}, {rot.y}, {rot.z}");
-        //
-        //     var pos2 = _cam.transform.position;
-        //     Debug.Log($"{pos2.x}, {pos2.y}, {pos2.z}");
-        //
-        //     var pos3 = _cam.transform.localPosition;
-        //     Debug.Log($"{pos3.x}, {pos3.y}, {pos3.z}");
-        // }
+
+        if (Input.GetMouseButtonDown(2))
+            Debug.Log("ScrollButton pressed!");
+        else if (Input.GetMouseButton(2))
+            _cam.transform.RotateAround(
+                player.transform.position,
+                new Vector3(0, 1.0f, 0),
+                10 * Input.GetAxis("Mouse X") * Time.deltaTime * cameraLookSpeed
+            );
     }
 }
